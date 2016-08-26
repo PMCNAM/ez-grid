ce5e0a747983ee7669f70d8654ff8ce138a1447a
Patrick McNamara <p.mcnamara20@gmail.com>
2016-08-26 00:52:52 -0400

    optimize minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,40 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		compass: {
+
+		sass: {
 			dist: {
 				options: {
-					sassDir: 'scss',
-					cssDir: 'css',
-					sourcemap: false,
-					outputStyle: 'compressed'
+					style: 'expanded',
+					precision: 3,
+					sourcemap: 'none'
+				},
+				files: {
+					'css/ezgrid.css': 'scss/ezgrid.scss'
 				}
 			}
 		},
+
+		cssmin: {
+			minify: {
+				src: 'css/ezgrid.css',
+				dest: 'css/min/ezgrid.min.css'
+			}
+		},
+
 		watch: {
 			css: {
-				files: 'scss/*.scss',
-				tasks: ['compass']
+				files: ['scss/*.scss'],
+				tasks: ['sass', 'cssmin']
 			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-compass');
+
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	
 	grunt.registerTask('default',['watch']);
+	grunt.registerTask('build', ['sass', 'cssmin']);
+
 }
